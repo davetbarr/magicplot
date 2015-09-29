5bade9f118bfac71bbae457ce4c0e15b78f14e0e
ojdfarley <o.j.d.farley@dur.ac.uk>
2015-09-29 12:32:49 +0100

    removed deleted files

--- plugins/Average.py
+++
@@ -1,9 +0,0 @@
-class Plugin(AnalysisPlugin):
-
-    def __init__(self):
-        AnalysisPlugin.__init__(self, params={},
-            name='Average')
-
-    def run(self):
-        return {'Average': numpy.average(self.data)}
-
