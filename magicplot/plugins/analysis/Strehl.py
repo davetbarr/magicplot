0d2d86058d215693bc0f0289a66bea4a6297d3d8
Ollie Farley <o.j.d.farley@dur.ac.uk>
2016-10-07 11:18:46 +0100

    strehl plugin changed default value of wavelength

--- magicplot/plugins/analysis/Strehl.py
+++ magicplot/plugins/analysis/Strehl.py
@@ -2,7 +2,7 @@ from scipy.stats import linregress
 class Plugin(AnalysisPlugin):
 
     def __init__(self):
-        AnalysisPlugin.__init__(self, params={'wavelength':.550, 'pixel_scale':1, 
+        AnalysisPlugin.__init__(self, params={'wavelength(um)':1.65, 'pixel_scale':1, 
                 'diameter':4.2,
                 'central_obsc':0},
                 name='Strehl')
@@ -10,7 +10,7 @@ class Plugin(AnalysisPlugin):
     def run(self):
         # () The normalisation - the expected 
         norm = ( 3.1415926/4.*(1.-self.params['central_obsc']**2) /
-                (0.206265*self.params['wavelength']/self.params['diameter']/self.params['pixel_scale'] )**2 )
+                (0.206265*self.params['wavelength(um)']/self.params['diameter']/self.params['pixel_scale'] )**2 )
         # () The total integral under the recorded PSF:
         if len(self.data) == 2:
             image = self.data[1]
